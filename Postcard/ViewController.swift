6da000b0514df01b4c0ac21aa242565726a5193a
Cheuk Him Ho <fathimpeter@fathims-iMac.local>
2014-10-17 21:46:08 +0800

    added comment

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -28,10 +28,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //code will evaluate when we press the button
+    
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
-        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
